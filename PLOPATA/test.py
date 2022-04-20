3b31a10ba30017d8a7ae96cdf43dce895f7f9ccd
PLopata <piotr.a.lopata@gmail.com>
2022-04-20 23:43:11 +0200

    TwoDimensions added, WIP

--- PLOPATA/test.py
+++ PLOPATA/test.py
@@ -5,11 +5,10 @@ import matplotlib.pyplot as plt
 
 
 # x = range(-10, 10, 2)
-x = np.linspace(0, 9, 10).tolist()
-y = []
-for i in x:
-    y.append(i*2 + 3)
+x1 = np.linspace(0, 9, 10)
+x2 = np.linspace(0, 9, 10)
 
-print(f"{y[x.index(4)]}")
+X, Y = np.meshgrid(x1, x2)
+print(X)
 # plt.plot(x, y)
 # plt.show()
