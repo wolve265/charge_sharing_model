7a183f63fd2d8a992ffcc62152d52bd47bfa188f
Krzysztof Cislo <krzysztof.cislo265@gmail.com>
2022-11-17 01:42:31 +0100

    fixed detectors dicts

--- KCISLO/detectors/cdte.py
+++ KCISLO/detectors/cdte.py
@@ -4,13 +4,13 @@ class CdTe:
         "charge_cloud_sigma": 16,
         "num_of_charges": 4970,
         "noise_sigma": 0
-    },
+    }
     actual = {
         "pixel_size": 100,
         "charge_cloud_sigma": 16,
         "num_of_charges": 4970,
         "noise_sigma": 200
-    },
+    }
     target = {
         "pixel_size": 50,
         "charge_cloud_sigma": 16,
--- KCISLO/detectors/si.py
+++ KCISLO/detectors/si.py
@@ -4,13 +4,13 @@ class SI:
         "charge_cloud_sigma": 8,
         "num_of_charges": 2200,
         "noise_sigma": 0
-    },
+    }
     actual = {
         "pixel_size": 75,
         "charge_cloud_sigma": 8,
         "num_of_charges": 2200,
         "noise_sigma": 45
-    },
+    }
     target = {
         "pixel_size": 50,
         "charge_cloud_sigma": 10,
