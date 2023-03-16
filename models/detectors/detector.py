2e72f96724f403670c0569b9bea3ba9eeb607811
Krzysztof Cislo <krzysztof.cislo265@gmail.com>
2023-03-16 19:20:46 +0100

    models:detector: added another string representation

--- models/detectors/detector.py
+++ models/detectors/detector.py
@@ -9,3 +9,9 @@ class Detector:
     noise_sigma: float
     name: str
     material: str
+
+    def get_str(self) -> str:
+        return (
+            f"""pixel size = {self.pixel_size}μm, charge cloud σ = {self.charge_cloud_sigma}μm\n"""
+            f"""number of charges = {self.num_of_charges}e, noise σ = {self.noise_sigma}e RMS"""
+        )
